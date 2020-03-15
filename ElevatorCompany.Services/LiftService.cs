2ca3c54944503ba4a08f78d1b13e6269289e0bb7
Dean <me@deantimson.com>
2020-03-15 11:55:02 +1000

    added Instruction, return an instruction based on current state of lift

--- ElevatorCompany.Services/LiftService.cs
+++ ElevatorCompany.Services/LiftService.cs
@@ -1,9 +1,28 @@
-﻿using System;
+﻿using ElevatorCompany.Models;
+using System;
 
 namespace ElevatorCompany.Services
 {
     public class LiftService
     {
+        public static Instruction CalculateNextInstruction(Lift lift)
+        {
+            Instruction instruction = Instruction.Stop;
 
+            switch (lift.State)
+            {
+                case Models.Enums.LiftState.Stopped:
+                    instruction = Instruction.Travel;
+                    break;
+                case Models.Enums.LiftState.DoorsOpen:
+                    instruction = Instruction.Stop; // close doors
+                    break;
+                case Models.Enums.LiftState.Moving:
+                    instruction = Instruction.Stop;
+                    break;
+            }
+
+            return instruction;
+        }
     }
 }
