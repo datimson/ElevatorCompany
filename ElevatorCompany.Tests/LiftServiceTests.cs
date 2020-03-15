4df92a1e4744ca5ca5878706de9b8efb0e5112bd
Dean <me@deantimson.com>
2020-03-15 16:09:49 +1000

    added tests for CalculateOptimalInstructions

--- ElevatorCompany.Tests/LiftServiceTests.cs
+++ ElevatorCompany.Tests/LiftServiceTests.cs
@@ -10,6 +10,170 @@ namespace ElevatorCompany.Tests
 {
     public class LiftServiceTests
     {
+        public class CalculateOptimalInstructions
+        {
+            [Theory]
+            [MemberData(nameof(Data))]
+            public void CanOptimiseInstructionsStartingAtGround(List<Summon> summons, List<Instruction> optimalInstructions)
+            {
+                var lift = new Lift { Level = 0 }; // 0 = Ground
+                var instructions = LiftService.CalculateOptimalInstructions(lift, summons);
+
+                Assert.Equal(instructions, optimalInstructions);
+            }
+
+            public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                /*
+                 * Passenger summons lift on the ground floor.
+                 * Once in, chooses to go to level 5.
+                 */
+                new object[] {
+                    new List<Summon>
+                    {
+                        new Summon(0, Direction.Up, new List<Passenger> { new Passenger(5) }),
+                    },
+                    new List<Instruction>
+                    {
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.Stop,
+                        Instruction.OpenDoors
+                    }
+                },
+                /*
+                 * Passenger summons lift on level 6 to go down.
+                 * A passenger on level 4 summons the lift to go down.
+                 * They both choose L1.
+                 */
+                new object[] {
+                    new List<Summon>
+                    {
+                        new Summon(6, Direction.Down, new List<Passenger> { new Passenger(1) }),
+                        new Summon(4, Direction.Down, new List<Passenger> { new Passenger(1) }),
+                    },
+                    new List<Instruction>
+                    {
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.Stop,
+                        Instruction.OpenDoors
+                    }
+                },
+                /*
+                 * Passenger 1 summons lift to go up from L2.
+                 * Passenger 2 summons lift to go down from L4.
+                 * Passenger 1 chooses to go to L6.
+                 * Passenger 2 chooses to go to Ground Floor
+                 */
+                new object[] {
+                    new List<Summon>
+                    {
+                        new Summon(2, Direction.Up, new List<Passenger> { new Passenger(6) }),
+                        new Summon(4, Direction.Down, new List<Passenger> { new Passenger(0) }),
+                    },
+                    new List<Instruction>
+                    {
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.Stop,
+                        Instruction.OpenDoors
+                    }
+                },
+                /*
+                 * Passenger 1 summons lift to go up from Ground.
+                 * They choose L5.
+                 * Passenger 2 summons lift to go down from L4.
+                 * Passenger 3 summons lift to go down from L10.
+                 * Passengers 2 and 3 choose to travel to Ground.
+                 */
+                new object[] {
+                    new List<Summon>
+                    {
+                        new Summon(0, Direction.Up, new List<Passenger> { new Passenger(5) }),
+                        new Summon(4, Direction.Down, new List<Passenger> { new Passenger(0) }),
+                        new Summon(10, Direction.Down, new List<Passenger> { new Passenger(0) })
+                    },
+                    new List<Instruction>
+                    {
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.TravelUp,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.Stop,
+                        Instruction.OpenDoors,
+                        Instruction.Stop,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.TravelDown,
+                        Instruction.Stop,
+                        Instruction.OpenDoors
+                    }
+                }
+            };
+        }
+
         public class CalculateNextInstruction
         {
             [Fact]
