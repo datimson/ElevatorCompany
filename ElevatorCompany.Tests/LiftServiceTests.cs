73c365e5ec97a6db6b6022a628cf09f7014d0f79
Dean <me@deantimson.com>
2020-03-15 12:21:03 +1000

    setup first test

--- ElevatorCompany.Tests/LiftServiceTests.cs
+++ ElevatorCompany.Tests/LiftServiceTests.cs
@@ -1,3 +1,6 @@
+using ElevatorCompany.Models;
+using ElevatorCompany.Models.Enums;
+using ElevatorCompany.Services;
 using System;
 using Xunit;
 
@@ -5,10 +8,28 @@ namespace ElevatorCompany.Tests
 {
     public class LiftServiceTests
     {
-        [Fact]
-        public void Test1()
+        public class CalculateNextInstruction
         {
+            [Fact]
+            public void When_Then()
+            {
 
+            }
+        }
+
+        public class ExecuteInstruction
+        {
+            [Fact]
+            public void WhenStop_ThenLiftStopped()
+            {
+                var lift = new Lift() {
+                    State = LiftState.Moving
+                };
+
+                LiftService.ExecuteInstruction(Instruction.Stop, lift);
+
+                Assert.True(lift.State == LiftState.Stopped);
+            }
         }
     }
 }
